f927d9a20667bc94df83ca0a31211304776a9eec
rifkiafnt12 <111101317+rifkiafnt12@users.noreply.github.com>
2022-08-20 07:12:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*========================= typing animation =========================*/
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "web Developer", "Grapic Designer", "Youtube"],
+  typeSpeed: 120,
+  BackSpeed: 60,
+  loop: true,
+});
